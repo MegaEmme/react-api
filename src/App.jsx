64d6d300dd51dcd072345e85a73513cb7baf2c30
MegaEmme <mikmusco@gmail.com>
2025-04-11 15:44:38 +0200

    feat: bonus 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,34 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Card from './components/Card';
-
 function App() {
-  const [actor, setActor] = useState([])
-  const endpointActresses = "https://www.freetestapi.com/api/v1/actresses"
-  const endpointActors = "https://www.freetestapi.com/api/v1/actors"
-
+  const [actor, setActor] = useState([]);
+  const endpointActresses = "https://www.freetestapi.com/api/v1/actresses";
+  const endpointActors = "https://www.freetestapi.com/api/v1/actors";
   function fetchActress() {
     axios.get(endpointActresses)
       .then(res => {
         setActor(res.data)
       })
-  }
-
+  };
   function fetchActors() {
     axios.get(endpointActors)
       .then(res => {
         setActor(res.data)
       })
-  }
-
-  console.log(actor)
-
-  useEffect(fetchActress, [])
-
-  useEffect(fetchActors, [])
-
+  };
+  console.log(actor);
+  // useEffect(fetchActress, []);
+  // useEffect(fetchActors, []);
   return (
     <>
       <h1>Lista Attori/Attrici</h1>
       <div className="buttons">
-        <button onClick={useEffect(fetchActress, [])}>Attori</button>
-        <button onClick={useEffect(fetchActors, [])}>Attrici</button>
+        <button onClick={fetchActors}>Attori</button>
+        <button onClick={fetchActress}>Attrici</button>
       </div>
       <Card Actor={actor} />
     </>
   )
-}
-
+};
 export default App;
