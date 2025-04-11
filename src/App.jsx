1e3f0f17b033e1f3bd70cda81752fedcd3aaf943
MegaEmme <mikmusco@gmail.com>
2025-04-11 14:19:17 +0200

    feat: milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-
+  const [actor, setActor] = useState([])
+  const endpoint = "https://www.freetestapi.com/api/v1/actresses"
+  function fetchActor() {
+    axios.get(endpoint)
+      .then(res => {
+        setActor(res.data)
+      })
+  }
+  console.log(actor)
+  useEffect(fetchActor, [])
   return (
     <>
+      <h1>Lista Attori/Attrici</h1>
     </>
   )
 }
